d2ccf8360bd47f198482fefcce9f895bdb039556
Igli milaqi <milaqiigli@gmail.com>
2019-10-15 12:43:21 +0200

    Added multi threaded calibration and working on the ui for easier testing.

--- New Unity Project/Assets/OpenCV/CalibrateCamera.cs
+++ New Unity Project/Assets/OpenCV/CalibrateCamera.cs
@@ -2,22 +2,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using OpenCvSharp;
 using OpenCvSharp.Aruco;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using static CalibrationThread;
+
+public class CalibrationThread
+{
+    public static void CalibrateAsync(int boardWidth, int boardHeight,ref List<List<Point3f>> objPoints,ref List<List<Point2f>> imagePoints, Mat mat,CalibrationData calibrationData)
+    {
+        Debug.Log("Calibrating Async.....");
+
+        if (CalibrateCamera.OnCalibrationStarted != null) CalibrateCamera.OnCalibrationStarted();
+        
+        double[,] k = new double[3, 3];
+        double[] d = new double[4];
+
+        Vec3d[] rvec = new Vec3d[boardWidth *  boardHeight];
+        Vec3d[] tvec = new Vec3d[boardWidth *  boardHeight];
+        
+        
+        Debug.Log("Error: " + Cv2.CalibrateCamera(objPoints, imagePoints, mat.Size(), k, d, out rvec, out tvec,
+                      CalibrationFlags.FixK4 | CalibrationFlags.FixK5,TermCriteria.Both(30,1)));
+        
+        calibrationData.RegisterMatrix(k);
+        Debug.Log(d[0] + " "+  d[1] + " " + d[2] + " "+  d[3]);
+        Debug.Log("Finished!!");
+
+        if (CalibrateCamera.OnCalibrationFinished != null)
+        {
+            CalibrateCamera.OnCalibrationFinished(calibrationData);
+        }
+    }
+}
+
 
 public class CalibrateCamera : WebCamera
 {
+    [Title("Calibration Settings",null,TitleAlignments.Centered)]
     public int boardWidth;
     public int boardHeight;
     public int squareSizeMilimeters;
 
     public CalibrationData calibrationData;
+    
+    [Title("Bool Variables",null,TitleAlignments.Centered)]
     public BoolVariable startCalibration;
-     public BoolVariable registerCalibFrame;
+    public BoolVariable registerCalibFrame;
+    
     private DetectorParameters detectorParameters;
     private Dictionary dictionary;
     private Mat mat;
@@ -29,6 +66,9 @@ public class CalibrateCamera : WebCamera
     
     private List<List<Point2f>>  imagePoints = new List<List<Point2f>>();
     private List<List<Point3f>> objPoints = new List<List<Point3f>>();
+
+    public static Action<CalibrationData> OnCalibrationFinished;
+    public static Action OnCalibrationStarted;
     
     protected override void Start()
     {
@@ -42,7 +82,7 @@ public class CalibrateCamera : WebCamera
         boardSize = new Size(boardWidth,boardHeight);
         
     }
-    
+
     private void RegisterCurrentCalib()
     {
         //corners.Clear();
@@ -84,6 +124,7 @@ public class CalibrateCamera : WebCamera
         Vec3d[] rvec = new Vec3d[boardWidth *  boardHeight];
         Vec3d[] tvec = new Vec3d[boardWidth *  boardHeight];
         
+        
         Debug.Log("Error: " + Cv2.CalibrateCamera(objPoints, imagePoints, mat.Size(), k, d, out rvec, out tvec,
             CalibrationFlags.FixK4 | CalibrationFlags.FixK5,TermCriteria.Both(30,1)));
         
@@ -114,9 +155,13 @@ public class CalibrateCamera : WebCamera
 
         if(startCalibration.value)
         {
-            Calibrate();
+            //Calibrate();
+            //boardWidth,boardHeight,ref objPoints,ref imagePoints,mat,calibrationData
+            Thread t = new Thread(() => CalibrateAsync(boardWidth,boardHeight,ref objPoints,ref imagePoints,mat,calibrationData));
+            t.Start();
             startCalibration.value = false;
         }
+     
             
         output = ARucoUnityHelper.MatToTexture(mat,output);
 
---
+++ New Unity Project/Assets/OpenCV/UpdateCalibrtionInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCalibrtionInfo : MonoBehaviour
+{
+    [SerializeField] private Button captureCalibButton;
+    [SerializeField] private Button calibrateButton;
+    [SerializeField] private Button finishCalibrationButton;
+
+    private bool finishedCalibration = false;
+    private bool startedCalibration = false;
+    private void Start()
+    {
+        CalibrateCamera.OnCalibrationFinished += OnCalibrationFinished;
+        CalibrateCamera.OnCalibrationStarted += OnCalibrationStarted;
+    }
+
+    private void Update()
+    {
+        finishCalibrationButton.gameObject.SetActive(finishedCalibration);
+        calibrateButton.interactable = !startedCalibration;
+    }
+
+    private void OnCalibrationFinished(CalibrationData data)
+    {
+        finishedCalibration = true;
+    }
+    
+    private void OnCalibrationStarted()
+    {
+        startedCalibration = true;
+    }
+
+    private void OnDisable()
+    {
+        CalibrateCamera.OnCalibrationFinished -= OnCalibrationFinished;
+        CalibrateCamera.OnCalibrationStarted -= OnCalibrationStarted;
+    }
+}
