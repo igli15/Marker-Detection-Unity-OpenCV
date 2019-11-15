27e483acb35b26d486c9ecd2ba9ffb24ac50b844
Igli milaqi <milaqiigli@gmail.com>
2019-11-15 23:01:25 +0100

    💡Camera Controller Now Works.

---
+++ New Unity Project/Assets/OpenCV/ARCameraController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+public class ARCameraController : MonoBehaviour
+{
+    public TrackedPoseDriver trackedPoseDriver;
+
+    private void Update()
+    {
+        Debug.Log(trackedPoseDriver.originPose.position);
+    }
+}
